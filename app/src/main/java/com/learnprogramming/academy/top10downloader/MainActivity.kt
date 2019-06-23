bd193f4153327c07a5a6eb77cf6ccb8c693d2110
Mubanga <mubangeriseme@yahoo.com>
2019-06-23 15:56:21 +0200

    AsyncTask and some logging for onPostExecute and doInBackground Added

--- app/src/main/java/com/learnprogramming/academy/top10downloader/MainActivity.kt
+++ app/src/main/java/com/learnprogramming/academy/top10downloader/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.learnprogramming.academy.top10downloader
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG,"OnCreate Called")
         // Wait Till I Get My Money Right
+    }
+
+    // Companion Objects Are Kotlin's Equivalents To Static
+    companion object {
+        private class DownloadData: AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG,"OnPostExecute Called With Parameter: ${result}")
+            }
+
+            override fun doInBackground(vararg params: String?): String? {
+                  // TODO("not implemented")To change body of created functions use File | Settings | File Templates.
+                Log.d(TAG,"doInBackground Called With Parameter: ${params[0]}")
+                return "doInBackground Complete"
+            }
+
+        }
     }
 }
